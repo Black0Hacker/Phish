7bfde639686b2ea48a1fc9b04f722d26536a74cd
ABOL_HACKER <abolfazlm3419.2@gmail.com>
2024-08-17 22:34:31 +0330

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+green='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+resetcollor='\e[0m'
+clear
+echo
+
+echo -e "\E[1;33m::::::::::::::::::: \e[97m   ABOL-HACKER  \E[1;33m::::::::::::::::::::"
+echo -e "ABOL-HACKER" |toilet |lolcat
+cd $HOME
+rm -rf ABOLHACKER
+git clone https://github.com/Black0Hacker/ABOLHACKER.git
+clear
+echo ""
+echo -e "$red [ $yellow + $red ] $green The script was updated $resetcollor"
+sleep 3.7
+echo ""
+cd $HOME
+cd ABOLHACKER
+clear
+echo -e "$red [ $yellow + $red ] $green the end"
